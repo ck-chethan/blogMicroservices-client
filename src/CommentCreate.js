ea43cefd713707453c7588255da65bada14ed193
Chethanck <45870776+Chethanck@users.noreply.github.com>
2021-11-01 17:52:37 +0530

    Client changes

---
+++ src/CommentCreate.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+const CommentCreate = ({ postId }) => {
+
+    const [content, setContent] = useState('');
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        });
+        setContent('');
+    }
+
+    return (
+        <div>
+            <form action="" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="commentAdd">New Comment</label>
+                    <input id="commentAdd" className="form-control" type="text" value={content} onChange={e => setContent(e.target.value)} />
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default CommentCreate
