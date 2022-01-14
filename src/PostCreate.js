b554b4a2be50f363e877a2060cf4dbcaf36db95d
chethanck <chethanckm66@gmail.com>
2022-01-14 14:55:55 +0530

    Paths changes

--- src/PostCreate.js
+++ src/PostCreate.js
@@ -1,30 +1,35 @@
-import React, { useState } from 'react'
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const PostCreate = () => {
+  const [title, setTitle] = useState('');
 
-    const [title, setTitle] = useState('');
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-    const onSubmit = async (event) => {
-        event.preventDefault();
+    await axios.post('http://posts.com/posts/create', {
+      title,
+    });
+    setTitle('');
+  };
 
-        await axios.post('http://localhost:4000/posts', {
-            title
-        });
-        setTitle('');
-    }
-
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
-                    <label htmlFor="postInput">Title</label>
-                    <input value={title} onChange={e => setTitle(e.target.value)} id="postInput" type="text" className="form-control" />
-                </div>
-                <button className="btn btn-primary">Submit</button>
-            </form>
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="postInput">Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            id="postInput"
+            type="text"
+            className="form-control"
+          />
         </div>
-    )
-}
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default PostCreate
+export default PostCreate;
